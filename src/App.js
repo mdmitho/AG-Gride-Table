e48ce1cb1d34d04b25fe1be1956c13a63ce55196
Hossen <mdmitho199911@gmail.com>
2022-06-28 23:05:36 +0600

    componentes doen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Table from './Components/Table/Table';
 
 function App() {
   return (
     <div className="App">
-        <h1 className=''>hello</h1>
+        <h1 className='font-bold text-5xl'>hello</h1>
+        <Table></Table>
     </div>
   );
-}
+}     
 
 export default App;
---
+++ src/Components/Table/Table.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Table.css'
+import { render } from 'react-dom';
+import { AgGridReact } from "ag-grid-react";
+
+const Table = () => {
+    return (
+        <div>
+            <h1>This is table</h1>
+        </div>
+    );
+};
+
+export default Table;
